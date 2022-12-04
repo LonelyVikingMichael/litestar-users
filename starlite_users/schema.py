268aaffb80bf3147b35fe57b69e8bd67eb86a894
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-04 14:55:08 +0200

    Update schemas

--- starlite_users/schema.py
+++ starlite_users/schema.py
@@ -1,20 +1,30 @@
 from typing import Optional, TypeVar
+from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, SecretStr
 
 
 class UserReadDTO(BaseModel):
+    id: UUID
     email: str
+
+    class Config:
+        orm_mode = True
 
 
 class UserCreateDTO(BaseModel):
     email: str
-    password: str
+    password: SecretStr
 
 
 class UserUpdateDTO(BaseModel):
     email: Optional[str]
-    password: Optional[str]
+    password: Optional[SecretStr]
+
+
+class UserAuthSchema(BaseModel):
+    email: str
+    password: SecretStr
 
 
 CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
