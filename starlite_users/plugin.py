ba0283833d3ea4c480565c9f54c8f6705da3b644
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-05 17:01:24 +0200

    Drop limited plugin interface

--- starlite_users/plugin.py
+++ starlite_users/plugin.py
@@ -1,17 +1,15 @@
-from typing import Any, Union, TYPE_CHECKING
-from uuid import UUID
+from typing import Any, Union, Optional, List, TYPE_CHECKING
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from starlite.plugins.base import PluginProtocol
 from starlite.security.session_auth import SessionAuth
 from starlite.contrib.jwt import JWTAuth
-from starlite import Router
+from starlite import Router, HTTPRouteHandler
 from starlite.middleware.session.base import BaseBackendConfig
 
-from .route_handlers import user_router
-
 if TYPE_CHECKING:
-    from starlite import Starlite
+    from starlite.config import AppConfig
+
 
 class StarliteUsersConfig(BaseModel):
     """Configuration for StarliteUsersPlugin."""
@@ -20,8 +18,8 @@ class StarliteUsersConfig(BaseModel):
         arbitrary_types_allowed = True
 
     auth_strategy: Union[SessionAuth, JWTAuth]
-    router: Router = user_router
     session_backend_config: Optional[BaseBackendConfig] = None
+    route_handlers: List[Union[HTTPRouteHandler, Router]]
 
 
 class StarliteUsersPlugin(PluginProtocol[Any]):
@@ -33,7 +31,11 @@ class StarliteUsersPlugin(PluginProtocol[Any]):
     ) -> None:
         self._config = config
 
-    def on_app_init(self, app: "Starlite") -> None:
-        if isinstance(self._config.auth_strategy, SessionAuth):
-            self._config.auth_strategy.session_backend_config = self._config.session_backend_config
-        app.register(self._config.router)
+    def on_app_init(self, app_config: "AppConfig") -> "AppConfig":
+        # if isinstance(self._config.auth_strategy, SessionAuth):
+        #     self._config.auth_strategy.session_backend_config = self._config.session_backend_config
+
+        app_config.route_handlers.extend(self._config.route_handlers)
+        app_config = self._config.auth_strategy.on_app_init(app_config)
+
+        return app_config
