386bb4422e46c58c8fb5ecfec7e12ac7ffd25f59
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-08 15:59:43 +0200

    Add plugin test

---
+++ tests/test_plugin.py
@@ -0,0 +1,13 @@
+from typing import TYPE_CHECKING
+
+from starlite_users.plugin import EXCLUDE_AUTH_HANDLERS
+
+if TYPE_CHECKING:
+    from starlite import Starlite
+
+
+def test_auth_exclude_paths(app: 'Starlite') -> None:
+    assert (
+        sorted(app.middleware[0].kwargs['config'].exclude)
+        == [f'/{handler}' for handler in sorted(EXCLUDE_AUTH_HANDLERS)]
+    )
