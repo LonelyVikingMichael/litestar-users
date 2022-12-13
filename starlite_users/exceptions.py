a299e4f8f592a64dcb1e74c236420f8da9591e8a
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-13 11:29:44 +0200

    Add token exception classes

--- starlite_users/exceptions.py
+++ starlite_users/exceptions.py
@@ -8,3 +8,15 @@ class UserNotFoundException(UserException):
 
 class UserConflictException(UserException):
     """Raise when db constraints are violated."""
+
+
+class TokenException(Exception):
+    """Base token exception."""
+
+
+class InvalidTokenException(TokenException):
+    """Raise when a JWT is found to be invalid."""
+
+
+class ExpiredTokenException(TokenException):
+    """Raise when a JWT is found to be expired."""
