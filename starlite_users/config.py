289722b3f9257e6f6b74ee1d4b17b5166547cdeb
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-13 11:29:29 +0200

    Add state setter and secret

--- starlite_users/config.py
+++ starlite_users/config.py
@@ -1,8 +1,8 @@
 from typing import Any, List, Generic, Literal, Optional, Type, Union
 
-from starlite import HTTPRouteHandler, Router
+from pydantic import BaseModel, SecretStr, root_validator
+from starlite import HTTPRouteHandler, Router, State
 from starlite.middleware.session.base import BaseBackendConfig
-from pydantic import BaseModel, root_validator
 
 from .models import UserModelType
 
@@ -16,6 +16,7 @@ class StarliteUsersConfig(BaseModel, Generic[UserModelType]):
     auth_exclude_paths: List[str] = []
     auth_strategy: Literal['session', 'jwt']
     route_handlers: List[Union[HTTPRouteHandler, Router]]
+    secret: SecretStr
     session_backend_config: Optional[BaseBackendConfig] = None
     user_model: Type[UserModelType]
 
@@ -24,3 +25,9 @@ class StarliteUsersConfig(BaseModel, Generic[UserModelType]):
         if values.get('auth_strategy') == 'session' and not values.get('session_backend_config'):
             raise ValueError('session_backend_config must be set when auth_strategy is set to "session"')
         return values
+
+    def _set_state(self, state: State):
+        state.starlite_users_config = {
+            'user_model': self.user_model,
+            'secret': self.secret,
+        }
