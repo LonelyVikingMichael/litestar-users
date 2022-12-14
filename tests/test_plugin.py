3be96fa38d8832bc326d430da107ff0c4ea63fed
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-14 22:50:38 +0200

    Fix test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -9,5 +9,5 @@ if TYPE_CHECKING:
 def test_auth_exclude_paths(app: 'Starlite') -> None:
     assert (
         sorted(app.middleware[0].kwargs['config'].exclude)
-        == [f'/{handler}' for handler in sorted(EXCLUDE_AUTH_HANDLERS)]
+        == [f'/{handler.replace("_", "-")}' for handler in sorted(EXCLUDE_AUTH_HANDLERS)]
     )
