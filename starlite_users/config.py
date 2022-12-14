3c9b39c2c857ecbfe8b62b31591cea08792135af
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-14 17:38:34 +0200

    Use config classes for route handler registration

--- starlite_users/config.py
+++ starlite_users/config.py
@@ -1,24 +1,72 @@
-from typing import Any, List, Generic, Literal, Optional, Type, Union
+from typing import Any, List, Generic, Literal, Optional, Tuple, Type, Union
 
 from pydantic import BaseModel, SecretStr, root_validator
 from starlite import HTTPRouteHandler, Router, State
 from starlite.middleware.session.base import BaseBackendConfig
 
 from .models import UserModelType
+from .schema import UserCreateDTOType, UserReadDTOType, UserUpdateDTOType
+
+
+class AuthHandlerConfig(BaseModel):
+    login_path: str = '/login'
+    logout_path: str = '/logout'
+
+
+class RegisterHandlerConfig(BaseModel):
+    path: str = '/register'
+
+
+class CurrentUserHandlerConfig(BaseModel):
+    path: str = '/users/me'
+
+
+class UserManagementHandlerConfig(BaseModel):
+    path_prefix: str = '/users'
+    authorized_roles: Tuple[str] = ('administrator',)
+
+
+class VerificationHandlerConfig(BaseModel):
+    path: str = '/verify'
+
+
+class PasswordResetHandlerConfig(BaseModel):
+    forgot_path: str = '/forgot-password'
+    reset_path: str = '/reset-password'
+
+
+class RouteHandlerConfig(BaseModel):
+    """The configurations provided are used to configure and register
+    Starlite-Users route handlers.
+    """
+
+    auth_handler_config: Optional[AuthHandlerConfig]
+    current_user_handler_config: Optional[CurrentUserHandlerConfig]
+    password_reset_handler_config: Optional[PasswordResetHandlerConfig]
+    register_handler_config: Optional[RegisterHandlerConfig]
+    user_management_handler_config: Optional[UserManagementHandlerConfig]
+    verification_handler_config: Optional[VerificationHandlerConfig]
+
+    @root_validator
+    def validate_config(cls, values: Any):
+        if all(value is None for value in values.values()):
+            raise ValueError('at least one route handler must be registered')
+        return values
 
 
 class StarliteUsersConfig(BaseModel, Generic[UserModelType]):
-    """Configuration for StarliteUsersPlugin."""
+    """Configuration class for StarliteUsersPlugin."""
 
     class Config:
         arbitrary_types_allowed = True
 
     auth_exclude_paths: List[str] = []
     auth_strategy: Literal['session', 'jwt']
-    route_handlers: List[Union[HTTPRouteHandler, Router]]
+    route_handler_config: RouteHandlerConfig
     secret: SecretStr
     session_backend_config: Optional[BaseBackendConfig] = None
     user_model: Type[UserModelType]
+    user_read_dto: Type[UserReadDTOType]
 
     @root_validator
     def validate_auth_backend(cls, values: Any):
--- starlite_users/plugin.py
+++ starlite_users/plugin.py
@@ -1,10 +1,17 @@
-from typing import Any, TYPE_CHECKING
+from typing import List, Union, TYPE_CHECKING
 
-from starlite.plugins.base import PluginProtocol
 from starlite import Router, HTTPRouteHandler, OpenAPIConfig
 from starlite.security.session_auth import SessionAuth
 
-from .config import StarliteUsersConfig
+from .config import StarliteUsersConfig, RouteHandlerConfig
+from .route_handlers import (
+    get_auth_handler,
+    get_current_user_handler,
+    get_password_reset_handler,
+    get_registration_handler,
+    get_user_management_handler,
+    get_verification_handler,
+)
 from .service import get_retrieve_user_handler
 
 if TYPE_CHECKING:
@@ -13,8 +20,8 @@ if TYPE_CHECKING:
 EXCLUDE_AUTH_HANDLERS = ('login', 'register', 'verify', 'forgot_password', 'reset_password')
 
 
-class StarliteUsersPlugin(PluginProtocol[Any]):
-    """A Plugin for authentication and user management."""
+class StarliteUsersPlugin:
+    """A Plugin for authentication, authorization and user management."""
 
     def __init__(
         self,
@@ -23,8 +30,9 @@ class StarliteUsersPlugin(PluginProtocol[Any]):
         self._config = config
 
     def on_app_init(self, app_config: "AppConfig") -> "AppConfig":
-        _auth_exclude_paths = set(*self._config.auth_exclude_paths)
-        for router in self._config.route_handlers:
+        _auth_exclude_paths = {*self._config.auth_exclude_paths}
+        
+        for router in self._get_route_handlers():
             if isinstance(router, Router):
                 for route in router.routes:
                     if any(name in EXCLUDE_AUTH_HANDLERS for name in route.handler_names):
@@ -48,3 +56,40 @@ class StarliteUsersPlugin(PluginProtocol[Any]):
         app_config.on_startup.append(self._config._set_state)
 
         return app_config
+
+    def _get_route_handlers(self) -> List[Union[HTTPRouteHandler, Router]]:
+        handlers = []
+        handler_config = self._config.route_handler_config
+        if handler_config.auth_handler_config:
+            handlers.append(get_auth_handler(
+                login_path=handler_config.auth_handler_config.login_path,
+                logout_path=handler_config.auth_handler_config.logout_path,
+                user_read_dto=self._config.user_read_dto,
+            ))
+        if handler_config.current_user_handler_config:
+            handlers.append(get_current_user_handler(
+                path=handler_config.current_user_handler_config.path,
+                user_read_dto=self._config.user_read_dto
+            ))
+        if handler_config.password_reset_handler_config:
+            handlers.append(get_password_reset_handler(
+                forgot_path=handler_config.password_reset_handler_config.forgot_path,
+                reset_path=handler_config.password_reset_handler_config.reset_path,
+            ))
+        if handler_config.register_handler_config:
+            handlers.append(get_registration_handler(
+                path=handler_config.register_handler_config.path,
+                user_read_dto=self._config.user_read_dto,
+            ))
+        if handler_config.user_management_handler_config:
+            handlers.append(get_user_management_handler(
+                path_prefix=handler_config.user_management_handler_config.path_prefix,
+                authorized_roles=handler_config.user_management_handler_config.authorized_roles,
+                user_read_dto=self._config.user_read_dto
+            ))
+        if handler_config.verification_handler_config:
+            handlers.append(get_verification_handler(
+                path=handler_config.verification_handler_config.path,
+                user_read_dto=self._config.user_read_dto,
+            ))
+        return handlers
