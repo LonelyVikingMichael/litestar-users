f8a1b174fe4571625056c850a20ae1e85ff59a9f
LonelyVikingMichael <michael@lonelyviking.com>
2022-12-24 19:05:34 +0200

    Add role tests

--- tests/test_admin_route_handlers.py
+++ tests/test_admin_route_handlers.py
@@ -17,10 +17,8 @@ GENERIC_USER_DICT = {
 
 @pytest.mark.usefixtures("mock_user_repository")
 class TestUserManagement:
-    def test_get_user_as_admin(
-        self, client: TestClient, admin_user: User, generic_user: User, mock_auth: MockAuth
-    ) -> None:
-        mock_auth.authenticate(admin_user.id)
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_get_user_as_admin(self, client: TestClient, generic_user: User) -> None:
         response = client.get(f"/users/{generic_user.id}")
         assert response.status_code == 200
         assert response.json() == GENERIC_USER_DICT
@@ -60,3 +58,32 @@ class TestUserManagement:
         mock_auth.authenticate(generic_user.id)
         response = client.delete(f"/users/{admin_user.id}")
         assert response.status_code == 401
+
+
+@pytest.mark.usefixtures("mock_user_repository")
+class TestRoleManagement:
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_create_role(self, client: TestClient) -> None:
+        response = client.post("/users/roles", json={"name": "editor", "description": "..."})
+        assert response.status_code == 201
+        assert response.json() == {
+            "id": ANY,
+            "name": "editor",
+            "description": "...",
+        }
+
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_update_role(self, client: TestClient) -> None:
+        pass
+
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_delete_role(self, client: TestClient) -> None:
+        pass
+
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_assign_role(self, client: TestClient) -> None:
+        pass
+
+    @pytest.mark.usefixtures("authenticate_admin")
+    def test_revoke_role(self, client: TestClient) -> None:
+        pass
