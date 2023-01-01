a176eda319dd539d64a462a540239820145ae5df
Michael Bosch <78952809+LonelyVikingMichael@users.noreply.github.com>
2023-01-01 17:53:58 +0200

    Fix plugin dependency key (#20)
    Fix SQLAlchemy session dependency key issue

--- starlite_users/dependencies.py
+++ starlite_users/dependencies.py
@@ -1,6 +1,10 @@
-from typing import Callable, Type
+from typing import Callable, Type, cast
 
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from starlite import State
+from starlite.plugins.sql_alchemy import SQLAlchemyPlugin
+from starlite.plugins.sql_alchemy.config import SESSION_SCOPE_KEY
+from starlite.types import Scope
 
 from starlite_users.adapter.sqlalchemy.mixins import RoleModelType, UserModelType
 from starlite_users.adapter.sqlalchemy.repository import SQLAlchemyUserRepository
@@ -17,12 +21,22 @@ def get_service_dependency(
         user_service_class: A subclass of [BaseUserService][starlite_users.service.BaseUserService]
     """
 
-    def get_service(session: AsyncSession) -> UserServiceType:
+    def get_service(scope: Scope, state: State) -> UserServiceType:
         """Instantiate service and repository for use with DI.
 
         Args:
-            session: SQLAlchemy AsyncSession
+            scope: ASGI scope
+            state: The application.state instance
         """
+        session = scope.get(SESSION_SCOPE_KEY)
+        for plugin in scope["app"].plugins:
+            if isinstance(plugin, SQLAlchemyPlugin):
+                config = plugin._config
+                break
+        if not session:
+            session_maker = cast("sessionmaker", state[config.session_maker_app_state_key])
+            session = scope[SESSION_SCOPE_KEY] = session_maker()
+
         return user_service_class(
             SQLAlchemyUserRepository(session=session, user_model_type=user_model, role_model_type=role_model)
         )
