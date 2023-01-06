2ba53b65dd011d8904991c826b56c96047165787
LonelyVikingMichael <michael@lonelyviking.com>
2023-01-06 17:22:31 +0200

    Improve code checks

--- tests/test_starlite_users.py
+++ tests/test_starlite_users.py
@@ -7,9 +7,11 @@ if TYPE_CHECKING:
 
 
 def test_auth_exclude_paths(app: "Starlite") -> None:
-    if app.middleware[0].kwargs.get("config"):
-        excluded_paths = app.middleware[0].kwargs["config"].exclude
-    elif app.middleware[0].kwargs.get("exclude"):
-        excluded_paths = app.middleware[0].kwargs["exclude"]
+    if hasattr(app.middleware[0], "kwargs") and app.middleware[0].kwargs.get("config"):  # pyright: ignore
+        excluded_paths = app.middleware[0].kwargs["config"].exclude  # pyright: ignore
+    elif hasattr(app.middleware[0], "kwargs") and app.middleware[0].kwargs.get("exclude"):  # pyright: ignore
+        excluded_paths = app.middleware[0].kwargs["exclude"]  # pyright: ignore
 
-    assert all(f'/{handler.replace("_", "-")}' in excluded_paths for handler in EXCLUDE_AUTH_HANDLERS)
+    assert all(
+        f'/{handler.replace("_", "-")}' in excluded_paths for handler in EXCLUDE_AUTH_HANDLERS  # pyright: ignore
+    )
