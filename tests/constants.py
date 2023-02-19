bf092d360c5e7ed9317e0e541014ba64877035fc
LonelyVikingMichael <michael@lonelyviking.com>
2023-02-19 19:11:58 +0200

    Update test to use const value

--- tests/constants.py
+++ tests/constants.py
@@ -1,3 +1,4 @@
 from pydantic import SecretStr
 
 ENCODING_SECRET = SecretStr("1234567890abcdef")
+DEFAULT_AUTH_EXCLUDE_PATHS = ("/login", "/register", "/verify", "/forgot-password", "/reset-password")
--- tests/test_starlite_users.py
+++ tests/test_starlite_users.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from starlite_users.main import EXCLUDE_AUTH_HANDLERS
+from tests.constants import DEFAULT_AUTH_EXCLUDE_PATHS
 
 if TYPE_CHECKING:
     from starlite import Starlite
@@ -12,6 +12,4 @@ def test_auth_exclude_paths(app: "Starlite") -> None:
     elif hasattr(app.middleware[0], "kwargs") and app.middleware[0].kwargs.get("exclude"):  # pyright: ignore
         excluded_paths = app.middleware[0].kwargs["exclude"]  # pyright: ignore
 
-    assert all(
-        f'/{handler.replace("_", "-")}' in excluded_paths for handler in EXCLUDE_AUTH_HANDLERS  # pyright: ignore
-    )
+    assert all(path in excluded_paths for path in DEFAULT_AUTH_EXCLUDE_PATHS)  # pyright: ignore
