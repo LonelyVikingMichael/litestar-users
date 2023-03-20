e064c5a2dac7fc286156a55d02ee050693e64a97
LonelyVikingMichael <michael@lonelyviking.com>
2023-03-20 15:24:47 +0200

    [test] remove unused patch

--- tests/test_role_integration/conftest.py
+++ tests/test_role_integration/conftest.py
@@ -143,6 +143,5 @@ def mock_user_repository(
     }
     monkeypatch.setattr(UserRepository, "user_store", user_store)
     monkeypatch.setattr(UserRepository, "role_store", role_store)
-    monkeypatch.setattr("starlite_users.service.SQLAlchemyUserRepository", UserRepository)
     monkeypatch.setattr("starlite_users.user_handlers.SQLAlchemyUserRepository", UserRepository)
     monkeypatch.setattr("starlite_users.dependencies.SQLAlchemyUserRepository", UserRepository)
