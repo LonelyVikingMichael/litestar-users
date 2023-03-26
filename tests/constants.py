7baaafdc606e8cbd54d6ebbc18a0b2102b189397
LonelyVikingMichael <michael@lonelyviking.com>
2023-03-26 18:29:23 +0200

    [clean] remove deprecated variable

--- tests/constants.py
+++ tests/constants.py
@@ -1,5 +1,4 @@
 from pydantic import SecretStr
 
 ENCODING_SECRET = SecretStr("1234567890abcdef")
-HASH_SCHEMES = ["bcrypt"]
-DEFAULT_AUTH_EXCLUDE_PATHS = ("/login", "/register", "/verify", "/forgot-password", "/reset-password")
+HASH_SCHEMES = ["argon2"]
