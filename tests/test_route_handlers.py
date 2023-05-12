0f102fd4d19f9f28c8579db7e4875996ed9f100d
LonelyVikingMichael <michael@lonelyviking.com>
2023-05-12 18:02:51 +0200

    handle unverified login attempts

--- tests/test_route_handlers.py
+++ tests/test_route_handlers.py
@@ -1,26 +1,40 @@
+from typing import TYPE_CHECKING
 from unittest.mock import ANY
 
 import pytest
 from pydantic import SecretStr
-from starlite.testing import TestClient
+from starlite.exceptions import NotAuthorizedException, PermissionDeniedException
 
 from starlite_users import StarliteUsersConfig
 
 from .conftest import User, password_manager
 from .utils import MockAuth
 
-
-@pytest.mark.usefixtures("mock_user_repository")
-def test_login(client: TestClient) -> None:
-    success_response = client.post("/login", json={"email": "admin@example.com", "password": "iamsuperadmin"})
-    assert success_response.status_code == 201
-
-    fail_response = client.post("/login", json={"email": "admin@example.com", "password": "ijustguessed"})
-    assert fail_response.status_code == 401
+if TYPE_CHECKING:
+    from starlite.testing import TestClient
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_logout(client: TestClient, generic_user: User, starlite_users_config: StarliteUsersConfig) -> None:
+class TestLogin:
+    def test_login(self, client: "TestClient") -> None:
+        response = client.post("/login", json={"email": "admin@example.com", "password": "iamsuperadmin"})
+        assert response.status_code == 201
+
+    @pytest.mark.xfail
+    def test_login_invalid_credentials(self, client: "TestClient") -> None:
+        with pytest.raises(NotAuthorizedException):
+            response = client.post("/login", json={"email": "admin@example.com", "password": "ijustguessed"})
+            assert response.status_code == 401
+
+    @pytest.mark.xfail
+    def test_login_unverified(self, client: "TestClient") -> None:
+        with pytest.raises(PermissionDeniedException):
+            response = client.post("/login", json={"email": "unverified@example.com", "password": "notveryverified"})
+            assert response.status_code == 403
+
+
+@pytest.mark.usefixtures("mock_user_repository")
+def test_logout(client: "TestClient", generic_user: User, starlite_users_config: StarliteUsersConfig) -> None:
     client.set_session_data({"user_id": str(generic_user.id)})
     response = client.post("/logout")
     if starlite_users_config.auth_backend != "session":
@@ -31,14 +45,14 @@ def test_logout(client: TestClient, generic_user: User, starlite_users_config: S
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_get_current_user(client: TestClient, generic_user: User, mock_auth: MockAuth) -> None:
+def test_get_current_user(client: "TestClient", generic_user: User, mock_auth: MockAuth) -> None:
     mock_auth.authenticate(generic_user.id)
     response = client.get("/users/me")
     assert response.status_code == 200
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_update_current_user(client: TestClient, generic_user: User, mock_auth: MockAuth) -> None:
+def test_update_current_user(client: "TestClient", generic_user: User, mock_auth: MockAuth) -> None:
     mock_auth.authenticate(generic_user.id)
     response = client.put("/users/me", json={"email": "updated@example.com"})
     assert response.status_code == 200
@@ -47,7 +61,7 @@ def test_update_current_user(client: TestClient, generic_user: User, mock_auth: 
 
 @pytest.mark.usefixtures("mock_user_repository")
 class TestRegistration:
-    def test_basic_registration(self, client: TestClient) -> None:
+    def test_basic_registration(self, client: "TestClient") -> None:
         response = client.post("/register", json={"email": "someone@example.com", "password": "something"})
         assert response.status_code == 201
         assert response.json() == {
@@ -57,11 +71,11 @@ class TestRegistration:
             "is_verified": False,
         }
 
-    def test_unique_email(self, client: TestClient, generic_user: User) -> None:
+    def test_unique_email(self, client: "TestClient", generic_user: User) -> None:
         response = client.post("/register", json={"email": generic_user.email, "password": "copycat"})
         assert response.status_code == 409
 
-    def test_unsafe_fields_unset(self, client: TestClient) -> None:
+    def test_unsafe_fields_unset(self, client: "TestClient") -> None:
         response = client.post(
             "/register",
             json={
@@ -81,7 +95,7 @@ class TestRegistration:
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_verification(client: TestClient, unverified_user: User, unverified_user_token: str) -> None:
+def test_verification(client: "TestClient", unverified_user: User, unverified_user_token: str) -> None:
     response = client.post("/verify", params={"token": unverified_user_token})
     assert response.status_code == 201
     response_body = response.json()
@@ -90,13 +104,13 @@ def test_verification(client: TestClient, unverified_user: User, unverified_user
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_forgot_password(client: TestClient, generic_user: User, monkeypatch: pytest.MonkeyPatch) -> None:
+def test_forgot_password(client: "TestClient", generic_user: User, monkeypatch: pytest.MonkeyPatch) -> None:
     response = client.post("/forgot-password", json={"email": generic_user.email})
     assert response.status_code == 201
 
 
 @pytest.mark.usefixtures("mock_user_repository")
-def test_reset_password(client: TestClient, generic_user: User, generic_user_password_reset_token: str) -> None:
+def test_reset_password(client: "TestClient", generic_user: User, generic_user_password_reset_token: str) -> None:
     PASSWORD = "veryverystrong123"
     response = client.post(
         "/reset-password",
